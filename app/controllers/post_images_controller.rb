eceafd01d89d266ab8c18e11bbf5b736e082bc50
Kohei Higashi <112879983+koheiino@users.noreply.github.com>
2023-04-24 08:45:05 +0000

    [Add]ページネーション

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -8,13 +8,17 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     #@post_image(投稿データ)のuser_idを、current_user.id(今ログインしているユーザーの ID)に指定することで投稿データに、今ログイン中のユーザーの ID を持たす
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
+
 
   def show
     @post_image = PostImage.find(params[:id])
