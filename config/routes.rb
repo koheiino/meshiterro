a1e7fa6082e950efba94a4b87898b9c2bb9b34ad
Kohei Higashi <112879983+koheiino@users.noreply.github.com>
2023-04-24 06:23:44 +0000

    [Add]削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
 
-  resources :post_images, only: [:new, :index, :show, :create]
+  resources :post_images, only: [:new, :index, :show, :create, :destory]
   get '/homes/about', to: 'homes#about', as: 'about'
 end
