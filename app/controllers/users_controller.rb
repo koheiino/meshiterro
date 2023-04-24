83ec9804862f86a90a9c381b660db12fbb33d5a9
Kohei Higashi <112879983+koheiino@users.noreply.github.com>
2023-04-24 07:22:14 +0000

    [Add]ユーザー情報編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = ユーザーの取得
+    @user.ユーザーのアップデート
+    redirect_to ユーザーの詳細ページへのパス  
+  end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
 end
